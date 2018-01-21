9540f2347bf46cdad3f1cb3904e054bee7823539
vibhaa <vibhaa@mit.edu>
2018-01-21 17:33:50 +0000

    fixing files and urls

--- generate_headers.sh
+++ generate_headers.sh
@@ -4,8 +4,8 @@ wget -r -np -R "index.html*" -p --save-headers http://128.30.10.12:8000/BigBuckB
 mv 128.30.10.12:8000/BigBuckBunny/* www.example3.org/BigBuckBunny/
 rm -rf 128.30.10.12\:8000/
 cd www.example3.org/BigBuckBunny
-for file in **/*; do sed -i "7iX-Original-URL: https://www.example3.org/BigBuckBunny/$file" $file; done
+for file in **/*; do sed -i "7iX-Original-URL: https://www.example3.org/$file" $file; done
 cp ../dash* .
 cp ../myindex* .
 for file in *; do sed -i "6d" $file; done
-for file in *; do sed -i "6iX-Original-URL: https://www.example3.org/BigBuckBunny/$file" $file; done
+for file in *; do sed -i "6iX-Original-URL: https://www.example3.org/$file" $file; done
