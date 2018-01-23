f941db86e286c55d881f1b238d4f6d43320fd16f
vibhaa <vibhaa@mit.edu>
2018-01-23 00:26:45 +0000

    new files and Seeking video

--- generate_headers.sh
+++ generate_headers.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # get files with HTTP headers
-mkdir www.example3.org/BigBuckBunny
-wget -r -np -R "index.html*" -p --save-headers http://128.30.10.12:8000/BigBuckBunny/ 
-mv 128.30.10.12:8000/BigBuckBunny/* www.example3.org/BigBuckBunny/
-rm -rf 128.30.10.12\:8000/
+mkdir www.example3.org/Seeking
+wget -r -np -R "index.html*" -p --save-headers http://128.52.168.245:8000/Seeking/ 
+mv 128.52.168.245:8000/Seeking/* www.example3.org/Seeking/
+rm -rf 128.52.168.245\:8000/
 
 # add url to all the header and chunk files
-cd www.example3.org/BigBuckBunny
-for file in **/*; do sed -i "7iX-Original-URL: https://www.example3.org/BigBuckBunny/$file" $file; done
+cd www.example3.org/Seeking
+for file in **/*; do sed -i "7iX-Original-URL: https://www.example3.org/Seeking/$file" $file; done
 
 # copy dash and index files and add url for them
 cp ../dash* .
 cp ../myindex* .
-for file in *; do sed -i "6iX-Original-URL: https://www.example3.org/BigBuckBunny/$file" $file; done
+for file in *; do sed -i "6iX-Original-URL: https://www.example3.org/Seeking/$file" $file; done
 
 # delete the 21st line
 for file in myindex_fastMPC*; do sed -i "21d" $file; done
 
 # replace it with new manifest url
-for file in myindex_fastMPC*; do sed -i "21i \ \ \ \ \t\t\tvar url = '/BigBuckBunny/Manifest.mpd';" $file; done
+for file in myindex_fastMPC*; do sed -i "21i \ \ \ \ \t\t\tvar url = '/Seeking/Manifest.mpd';" $file; done
 
 # hardcode video file in
-for file in myindex_fastMPC*; do sed -i "51i \ \ \ \ \t\t\tplayer.setVideoFile('BigBuckBunny')" $file; done
+for file in myindex_fastMPC*; do sed -i "51i \ \ \ \ \t\t\tplayer.setVideoFile('Seeking')" $file; done
