393d986bd032395a46d15e4de8a13f6836a9a7c5
arc <addanki@mit.edu>
2018-01-23 15:05:56 +0000

    Make generate headers script more generic

--- generate_headers.sh
+++ generate_headers.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
-# get files with HTTP headers
-mkdir www.example3.org/Tennis
-wget -r -np -R "index.html*" -p --save-headers http://128.52.168.245:8000/Tennis/ 
-mv 128.52.168.245:8000/Tennis/* www.example3.org/Tennis/
-rm -rf 128.52.168.245\:8000/
+for vid in BigBuckBunny  BirdsInCage  CrowdRun  ElFuente1  ElFuente2  FoxBird  OldTownCross  Seeking  Tennis
+do
+	vid_dir=preprocessed_vids/$vid/
+	server=localhost\:8000 # vid_dir=www.example3.org/$vid/
+	
+	mkdir $vid_dir
+	# get files with HTTP headers
+	wget -r -np -R "index.html*" -p --save-headers http://$server/$vid/
+	mv $server/$vid/* $vid_dir
+	rm -rf $server/
 
-# add url to all the header and chunk files
-cd www.example3.org/Tennis
-for file in **/*; do sed -i "7iX-Original-URL: https://www.example3.org/Tennis/$file" $file; done
+	# add url to all the header and chunk files
+	cd $vid_dir
+	for file in **/*; do sed -i "7iX-Original-URL: https://www.example3.org/$vid/$file" $file; done
 
-# copy dash and index files and add url for them
-cp ../dash* .
-cp ../myindex* .
-for file in *; do sed -i "6iX-Original-URL: https://www.example3.org/Tennis/$file" $file; done
+	# copy dash and index files and add url for them
+	cp ../dash* .
+	cp ../myindex* .
+	for file in *; do sed -i "6iX-Original-URL: https://www.example3.org/$vid/$file" $file; done
 
-# delete the 21st line
-for file in myindex_fastMPC*; do sed -i "21d" $file; done
+	# delete the 21st line
+	for file in myindex_fastMPC*; do sed -i "21d" $file; done
 
-# replace it with new manifest url
-for file in myindex_fastMPC*; do sed -i "21i \ \ \ \ \t\t\tvar url = '/Tennis/Manifest.mpd';" $file; done
+	# replace it with new manifest url
+	for file in myindex_fastMPC*; do sed -i "21i \ \ \ \ \t\t\tvar url = '/$vid/Manifest.mpd';" $file; done
 
-# hardcode video file in
-for file in myindex_fastMPC*; do sed -i "51i \ \ \ \ \t\t\tplayer.setVideoFile('Tennis')" $file; done
+	# hardcode video file in
+	for file in myindex_fastMPC*; do sed -i "51i \ \ \ \ \t\t\tplayer.setVideoFile('$vid')" $file; done
 
-# vid_name="Seeking"
-# ip="128.52.168.245:8000"
-# mkdir www.example3.org/$vid_name
-# wget -r -np -R "index.html*" -p --save-headers http://$ip/$vid_name/ 
-# mv $ip/$vid_name/* www.example3.org/$vid_name/
-# rm -rf $ip/
-# cd www.example3.org/$vid_name
-# for file in **/*; do sed -i "7iX-Original-URL: https://www.example3.org/$vid_name/$file" $file; done
-# cp ../dash* .
-# cp ../myindex* .
-# for file in *; do sed -i "6d" $file; done
-# for file in *; do sed -i "6iX-Original-URL: https://www.example3.org/$vid_name/$file" $file; done
+	cd -
+
+done
