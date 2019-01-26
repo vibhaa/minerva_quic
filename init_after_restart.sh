7e8e6379d175316e6b1208b6a29348941525f3a7
vikram <vikramn@mit.edu>
2019-01-26 00:13:14 +0000

    helpful prints during init :)

--- init_after_restart.sh
+++ init_after_restart.sh
@@ -5,7 +5,7 @@ echo "Configuring Mahimahi..."
 sudo sysctl -w net.ipv4.ip_forward=1
 
 # Remount efs
-echo "Mounting NFS on ~/efs..."
+echo "Mounting NFS on ~/efs. This will take some time..."
 sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-042c304d.efs.us-east-1.amazonaws.com:/ ~/efs
 sudo chown -R ubuntu ~/efs
 
@@ -18,6 +18,7 @@ mkdir -p /tmp/quic-data/www.example3.org
 echo "Copying QUIC server data from NFS"
 if [ ! -d "~/all_quic_data" ];
 then
+    echo "Copying videos. This will take some more time..."
     cp ~/efs/all_quic_data ~/all_quic_data
 fi
 
